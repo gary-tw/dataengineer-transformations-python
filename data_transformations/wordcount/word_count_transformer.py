b5b6d81bb59eb2ec4b73adeee02957787ea29973
Gary Ng <gary.ng@thoughtworks.com>
2025-07-03 12:39:14 +0800

    Pass test_word_count

--- data_transformations/wordcount/word_count_transformer.py
+++ data_transformations/wordcount/word_count_transformer.py
@@ -1,6 +1,7 @@
 import logging
 
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
 
 
 def run(spark: SparkSession, input_path: str, output_path: str) -> None:
@@ -9,4 +10,16 @@ def run(spark: SparkSession, input_path: str, output_path: str) -> None:
 
     logging.info("Writing csv to directory: %s", output_path)
 
-    input_df.coalesce(1).write.csv(output_path, header=True)
+    output_df = (
+        input_df
+        .withColumn("value", F.lower(F.col("value")))
+        .withColumn("value", F.regexp_replace(F.col("value"), "[^\w\s+']", " "))
+        .withColumn("word", F.explode(F.split(F.col("value"), "\s+")))
+        .drop("value")
+        .filter(F.length(F.col("word")) > 0)
+        .groupby("word")
+        .count()
+        .orderBy("word")
+    )
+
+    output_df.coalesce(1).write.csv(output_path, header=True)
